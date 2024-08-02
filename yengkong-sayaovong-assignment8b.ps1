5f020ab313a78f64236dccfcd4a9adf1ff2f1fb9
YSayaovong <110697608+YSayaovong@users.noreply.github.com>
2024-08-02 06:40:44 -0500

    Add files via upload

---
+++ yengkong-sayaovong-assignment8b.ps1
@@ -0,0 +1,12 @@
+PS C:\Windows\system32> New-Item "C:\SharedFolder" -Type Directory
+
+
+    Directory: C:\
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----         9/30/2022   9:16 PM                SharedFolder
+
+
+PS C:\Windows\system32>
